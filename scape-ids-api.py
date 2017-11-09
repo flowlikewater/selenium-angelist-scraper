30c0a82db55cd34cf62aff822f9e72a2204c3f44
Kevin <cho.kevin@hotmail.com>
2017-11-09 15:08:02 +0800

    able to scrape by multiple keywords

--- scape-ids-api.py
+++ scape-ids-api.py
@@ -1,20 +1,26 @@
 import json, requests
 
-def findIds(page):
+def findIds(page, array):
+    payloadKeywords = ''
+    for i in range(len(array)):
+        payloadKeywords += '"filter_data[keywords]":"' + str(array[i]) + '",'
+    payloadKeywords =  '{' + payloadKeywords + '"sort":"joined",' + '"page":' + str(page) + '}'
+    print(payloadKeywords)
     url = 'https://angel.co/company_filters/search_data'
     headers = {'X-Requested-With': 'XMLHttpRequest'}
-    payload = {
-        'filter_data[keywords]':'ai',
-        'sort':'joined',
-        'filter_data[keywords]':'asia',
-        'page':page
-    }
+    payload = json.loads(payloadKeywords)
     r = requests.post(url, headers=headers, data=payload)
     data = json.loads(r.text)
+    print data
     print data['ids']
 
 def run20Times():
-    for i in range(20):
-        findIds(i)
+    for i in range(2):
+        findIds(i, ['AI', 'Asia'])
 
-run20Times()
+keywords_to_scrape = [['AI'],['AI', 'Asia'],['AI', 'Japan']]
+
+# run20Times()
+
+findIds(1,['AI', 'Asia'])
+findIds(2,['AI', 'Asia'])
