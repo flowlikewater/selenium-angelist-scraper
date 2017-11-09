6972c1ab190879a4e98248748212810f2e125d59
Kevin <cho.kevin@hotmail.com>
2017-11-09 12:22:14 +0800

    scrapes all pages for 20 times

--- scape-ids-api.py
+++ scape-ids-api.py
@@ -1,13 +1,20 @@
 import json, requests
 
-url = 'https://angel.co/company_filters/search_data'
-headers = {'X-Requested-With': 'XMLHttpRequest'}
-payload = {
-    'filter_data[keywords]':'ai',
-    'sort':'joined',
-    'filter_data[keywords]':'asia',
-    'page':'1'
-}
-r = requests.post(url, headers=headers, data=payload)
-data = json.loads(r.text)
-print data['ids']
+def findIds(page):
+    url = 'https://angel.co/company_filters/search_data'
+    headers = {'X-Requested-With': 'XMLHttpRequest'}
+    payload = {
+        'filter_data[keywords]':'ai',
+        'sort':'joined',
+        'filter_data[keywords]':'asia',
+        'page':page
+    }
+    r = requests.post(url, headers=headers, data=payload)
+    data = json.loads(r.text)
+    print data['ids']
+
+def run20Times():
+    for i in range(20):
+        findIds(i)
+
+run20Times()
